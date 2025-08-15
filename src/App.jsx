8955c9e41f1dfbb39aba0bb066974b6171fee49e
KennethVelazquez <kenny.arcelay@gmail.com>
2025-08-15 16:38:52 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './style.css'
+import Header from "/src/components/Header.jsx"
+import Main from "/src/components/Main.jsx"
+import Footer from "/src/components/Footer.jsx"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+    return (
+        <>
+            <main className="main">
+                <section className="main__text">
+                    <h2 className="about__h">About</h2>
+                    <p className="about__p">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+                    <h2 className="interests__h">Interests</h2>
+                    <p className="interests__p">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+                </section>
+            </main>
+        </>
+    )
+}
