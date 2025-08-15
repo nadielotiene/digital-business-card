e514f23f48cb077c13da0f7a29a59e3603415dff
KennethVelazquez <kenny.arcelay@gmail.com>
2025-08-15 17:03:21 -0400

    images route fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ export default function Footer() {
     return (
         <>
             <footer className="footer">
-                <img className="twitter__img footer__img" src="/public/twitter-icon.png" alt="twitter icon" />
-                <img className="facebook__img footer__img" src="/public/facebook-icon.png" alt="facebook icon" />
-                <img className="instagram__img footer__img" src="/public/instagram-icon.png" alt="instagram icon" />
-                <img className="github__img footer__img" src="/public/gitHub-icon.png" alt="github icon" />
+                <img className="twitter__img footer__img" src="twitter-icon.png" alt="twitter icon" />
+                <img className="facebook__img footer__img" src="facebook-icon.png" alt="facebook icon" />
+                <img className="instagram__img footer__img" src="instagram-icon.png" alt="instagram icon" />
+                <img className="github__img footer__img" src="gitHub-icon.png" alt="github icon" />
             </footer>
         </>
     )
